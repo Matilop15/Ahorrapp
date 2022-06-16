f1b649e30f038ac0e7d5e3c66f6bf5a278d28599
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-06-16 00:19:27 -0300

    refactor: Change cart icon

--- front-end/components/Phone/Nav.vue
+++ front-end/components/Phone/Nav.vue
@@ -3,8 +3,8 @@
     <button>
       <NuxtLink to="/mi-lista">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-        </svg>
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+      </svg>
         <span class="tag">Mi Lista</span>
       </NuxtLink>
     </button>
