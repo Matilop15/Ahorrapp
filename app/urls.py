3bd6c771c3f20c3901ffeb5516e4606150df01b2
AngelPiva <angelpiva6@gmail.com>
2022-06-18 12:10:53 -0300

    fixed ReadOnly requests

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from .views import ProductMarketViewSet, ProductViewSet, SuperMarketViewSet
 """ defines URLs """
@@ -7,6 +7,7 @@ router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'supermarkets', SuperMarketViewSet)
 router.register(r'productmarkets', ProductMarketViewSet)
+# router.register(r'products-supermarket', AllProductsViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
+from urllib import request
 from rest_framework import viewsets
+from django.http import JsonResponse
 from .serializers import ProductSerializer
 from .serializers import ProductMarketSerializer
 from .serializers import SuperMarketSerializer
@@ -6,23 +8,32 @@ from .models import Product, ProductMarket, SuperMarket
 """ Defines views """
 
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     """ Product View Set """
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
 
-class SuperMarketViewSet(viewsets.ModelViewSet):
+class SuperMarketViewSet(viewsets.ReadOnlyModelViewSet):
     """ Super Market View Set """
     serializer_class = SuperMarketSerializer
     queryset = SuperMarket.objects.all()
 
 
-class ProductMarketViewSet(viewsets.ModelViewSet):
+class ProductMarketViewSet(viewsets.ReadOnlyModelViewSet):
     """ Product Market View set """
     serializer_class = ProductMarketSerializer
     queryset = ProductMarket.objects.all()
 
-# class AllProductsViewSet(request):
+
+# class AllProductsViewSet(viewsets.ViewSet):
     # """ All products from a supermarket"""
-    # if request.method == 'GET':
+
+    # def retrieve(self, request, *args, **kwargs):
+        # queryParams = self.request.GET.get('abc')
+        # inner_qs = ProductMarket.objects.filter(SuperMarketId_id=queryParams)
+        # entries = Product.objects.filter(ProductId__in=inner_qs)
+        # queryset = Product.objects.filter(ProductId = queryParams)
+        # serializer = PlayerSerializer(queryset)
+        # return Response(serializer.data)
+
