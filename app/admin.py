8fae26cd2b042086e7f0579be1cf0f8c84489f87
sahinmeric <sahin.meric86@gmail.com>
2022-06-19 18:24:12 -0300

    db models refactored and admin panel fix

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import brand, product, market
+from .models import product_list, sub_category, category
+
+
+@admin.register(product)
+class product_Admin(admin.ModelAdmin):
+    list_display = ['product_id', 'product_url', 'price',
+                    'market_id', 'update_date']
+
+
+@admin.register(market)
+class market_Admin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(product_list)
+class product_list_Admin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'img_url', 'brand_id', 'cat_id', 'sub_id']
+
+
+@admin.register(category)
+class category_Admin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(sub_category)
+class sub_category_Admin(admin.ModelAdmin):
+    list_display = ['id', 'cat_id', 'name']
+
+
+@admin.register(brand)
+class brand_Admin(admin.ModelAdmin):
+    list_display = ['id', 'name']
