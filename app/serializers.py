de29ec43f68f5f6dff2bdfd461e6c8decc3776cb
AngelPiva <angelpiva6@gmail.com>
2022-06-21 08:43:00 -0300

    fixed APIs

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,7 @@
 from pyexpat import model
 from rest_framework import serializers
-from app.models import product_list, product, market, category, sub_category, brand
+from .models import product_list, product, market
+from .models import category, sub_category, brand
 """ serializers """
 
 
@@ -9,40 +10,41 @@ class SuperMarketSerializer(serializers.ModelSerializer):
     class Meta:
         """class Meta"""
         model = market
-        fields = ('Name', 'url')
+        fields = '__all__'
 
 
 class product_listSerializer(serializers.ModelSerializer):
     """class product_list Serializer"""
     class Meta:
         """class Meta"""
-        model = product
-        fields = ('id', 'name', 'img_url',
-                  'brand_id', 'cat_id', 'sub_id')
+        model = product_list
+        fields = '__all__'
 
 
 class productSerializer(serializers.ModelSerializer):
     """class product serializer"""
     class Meta:
         """class Meta"""
-        model = product_list
-        fields = ('product_id', 'price', 'product_url',
-                  'market_id', 'update_date')
+        model = product
+        fields = '__all__'
+
 
 class categorySerializer(serializers.ModelSerializer):
     """class category serializer"""
     class Meta:
         model = category
-        fields = ('name')
+        fields = '__all__'
+
 
 class sub_categorySerializer(serializers.ModelSerializer):
     """ class sub-category serializer """
     class Meta:
         model = sub_category
-        fields = ('name', 'cat_id')
+        fields = '__all__'
+
 
 class brandSerializer(serializers.ModelSerializer):
     """ class brand serializer """
     class Meta:
         model = brand
-        fields = ('name')
+        fields = '__all__'
