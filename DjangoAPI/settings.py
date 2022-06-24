d976a68ef181ec54019ed809d8a13961839a91aa
sahinmeric <sahin.meric86@gmail.com>
2022-06-24 13:32:17 -0300

    trusted origin *

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -27,7 +27,7 @@ ALLOWED_HOSTS = [
     'www.ahorrapp.me',
     '127.0.0.1'
 ]
-CSRF_TRUSTED_ORIGINS = ['https://www.ahorrapp.me/api/admin']
+CSRF_TRUSTED_ORIGINS = ['https://*.ahorrapp.me']
 
 # Application definition
 
